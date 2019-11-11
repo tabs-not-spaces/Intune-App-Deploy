a21c7eb244a2ed5817fa5e97e2a97d75b2f6cacd
Ben Reader <breader@vigilant.it>
2019-11-11 14:12:36 +1100

    fix: adding back in accidentally deleted function

--- tasks/Deploy.Functions.ps1
+++ tasks/Deploy.Functions.ps1
@@ -197,6 +197,36 @@ function Send-FileToAzureStorage {
     }
 
 }
+function Wait-ForFileProcessing {
+    [cmdletbinding()]
+    param (
+        $fileUri,
+        $stage
+    )
+    $attempts = 600
+    $waitTimeInSeconds = 10
+    $successState = "$($stage)Success"
+    $pendingState = "$($stage)Pending"
+    $failedState = "$($stage)Failed"
+    $timedOutState = "$($stage)TimedOut"
+    $file = $null
+    while ($attempts -gt 0) {
+        $file = New-GetRequest $fileUri
+        if ($file.uploadState -eq $successState) {
+            break
+        }
+        elseif ($file.uploadState -ne $pendingState) {
+            Write-Host -ForegroundColor Red $_.Exception.Message
+            throw "File upload state is not success: $($file.uploadState)"
+        }
+        Start-Sleep $waitTimeInSeconds
+        $attempts--
+    }
+    if ($file -eq $null -or $file.uploadState -ne $successState) {
+        throw "File request did not complete in the allotted time."
+    }
+    $file
+}
 function Get-Win32AppBody {
     param
     (
